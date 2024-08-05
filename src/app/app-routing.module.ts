ebc0d2ee3ed49cd6f3848cf1c7c938ff176c714f
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-08-05 15:47:14 -0500

    Inicializo los componentes que planeo usar y arreglo las rutas que quiero usar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SectionComponent } from './pages/section/section.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "product",
+    component: ProductComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "search",
+    component: SearchComponent
+  },
+  {
+    path: "section",
+    component: SectionComponent
+  },
+  {
+    path: "shoppingCart",
+    component: ShoppingCartComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/interfaces/profile.ts
@@ -0,0 +1,6 @@
+interface Profile{
+    name: string,
+    direction: string,
+    deliveryDetails: string,
+    phone?: string
+}
