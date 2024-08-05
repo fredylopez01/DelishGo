092c2c87a468086aaf48adf20347d6e1b4a29794
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-08-05 17:47:35 -0500

    Diseño y pequeño funcionamiento de la barra del menu

---
+++ src/app/core/components/tabs/tabs.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss']
+})
+export class TabsComponent {
+  status = [false, false, false, false];
+  activeColor = "#000";
+  disabledColor = "#555";
+
+  constructor(private router: Router){
+    this.router.events.subscribe(event => {
+      console.log(event)
+      if(event instanceof NavigationEnd){
+        switch (event.urlAfterRedirects){
+          case "/":
+            this.status = [true, false, false, false];
+            break;
+          case "/search":
+            this.status = [false, true, false, false];
+            break;
+          case "/shoppingCart":
+            this.status = [false, false, true, false];
+            break;
+          case "/profile":
+            this.status = [false, false, false, true];
+            break;
+          default:
+            this.status = [true, false, false, false];
+            break;
+        }
+      }
+    })
+  }
+
+  browse(direction:string){
+    this.router.navigate([direction]);
+  }
+
+}
