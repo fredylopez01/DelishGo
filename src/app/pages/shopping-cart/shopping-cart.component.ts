113898046a4255d8bd12d4273528f72e19e5e620
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2024-08-12 11:13:35 -0500

    Doy diseño y buena apariencia a la lista de productos que esta en el carrito

--- src/app/pages/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -3,6 +3,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service';
 import { ProductCounterComponent } from "../../core/components/product-counter/product-counter.component";
+import { Product } from 'src/app/core/interfaces/product';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -14,9 +16,15 @@ import { ProductCounterComponent } from "../../core/components/product-counter/p
 export class ShoppingCartComponent implements OnInit {
   serviceHeader = inject(HeaderService);
   shoppingCartService = inject(ShoppingCartService);
+  products:Product[] =[];
+  productService = inject(ProductService);
 
   ngOnInit(): void {
     this.serviceHeader.title.set("Carrito de Compras");
+    this.shoppingCartService.shoppingCart.forEach(async item =>{
+      const ans = await this.productService.getProductById(item.idProduct);
+      if(ans) this.products.push(ans);
+    })
   }
 
   deleteProduct(id:number) {
